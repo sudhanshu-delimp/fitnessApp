9f503f2a30df7e9fc2279e1eab279645fcccd241
Sudhanshu Bisht <sudhanshu@delimp.com>
2021-07-27 19:18:16 +0530

    admin module and apis

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: '',
     database: process.env.DB_NAME
 });
 
