339114308d431dc5225a19af2f147a525d0cccec
Sudhanshu Bisht <sudhanshu@delimp.com>
2021-08-09 18:37:44 +0530

    remove vieo from aws

--- helpers/video.js
+++ helpers/video.js
@@ -41,7 +41,8 @@ exports.deleteFile =  async (destination_path) => {
         accessKeyId: process.env.ACCESS_KEY_ID,
         secretAccessKey: process.env.SECRET_ACCESS_KEY
       });
-      let aws_path = destination_path.replace('https://axces.s3.ap-southeast-2.amazonaws.com/','')
+      var aws_path = destination_path.replace('https://axces.s3.ap-southeast-2.amazonaws.com/','');
+      aws_path = destination_path.replace('https://axces.s3.amazonaws.com/','');
       console.log(aws_path);
       const params = {
         Bucket: process.env.BUCKET_NAME,
